a4a405bd94389ac7f3e782717e973d92bcd8b732
federica100 <105656784+federica100@users.noreply.github.com>
2023-05-23 14:21:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+
+const imgElement = document.querySelector('.hover-image');
+const defaultSrc = imgElement.getAttribute('src');
+const hoverSrc = imgElement.getAttribute('data-hover-src');
+
+imgElement.addEventListener('mouseenter', () => {
+  imgElement.setAttribute('src', hoverSrc);
+});
+
+imgElement.addEventListener('mouseleave', () => {
+  imgElement.setAttribute('src', defaultSrc);
+});
+
+
+
+
+
+function visualizzaTaglie() {
+  var taglie = document.getElementById("taglie");
+  var taglieup = document.getElementById("taglieup");
+  var tagliedown = document.getElementById("tagliedown");
+  if (taglie.style.display === "none") {
+    taglie.style.display = "block";
+    taglieup.style.display = "block";
+    tagliedown.style.display = "none";
+  } else {
+    taglie.style.display = "none";
+    taglieup.style.display = "none";
+    tagliedown.style.display = "block";
+  }
+}
+function cambiaTaglia() {
+  var taglia = document.getElementById("taglia");
+  taglia.value = event.target.innerHTML;
+}
+
+
+
+
+
